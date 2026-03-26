9149cc42302a12f61d82b94314c5dde2c193bf95
Matias Chomali <matiaschomali@users.noreply.github.com>
2026-03-26 10:06:24 -0300

    Hardcode Azure redirect URI for production

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const msalConfig = {
   auth: {
     clientId: '363fd41d-fb01-40f5-9cb8-8162bdabf596',
     authority: 'https://login.microsoftonline.com/70db351d-e90a-41dc-9eea-1a72589d1d95',
-    redirectUri: `${window.location.origin}/maritex-dashboard/`,
+    redirectUri: 'https://choma1997.github.io/maritex-dashboard/',
   },
   cache: {
     cacheLocation: 'sessionStorage',
