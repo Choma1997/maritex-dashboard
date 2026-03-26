17b825910da4e5353f13dc2aa23b8c6ee8d22241
Matias Chomali <matiaschomali@users.noreply.github.com>
2026-03-26 09:56:25 -0300

    Set Azure client and tenant IDs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import {
   Loader2,
   Upload,
@@ -19,8 +19,8 @@ import {
  */
 const msalConfig = {
   auth: {
-    clientId: 'PONER_AQUI_CLIENT_ID',
-    authority: 'https://login.microsoftonline.com/PONER_AQUI_TENANT_ID',
+    clientId: '363fd41d-fb01-40f5-9cb8-8162bdabf596',
+    authority: 'https://login.microsoftonline.com/70db351d-e90a-41dc-9eea-1a72589d1d95',
     redirectUri: window.location.origin,
   },
   cache: {
